3dae5de191acd139496c10264e5ff21dc18b6690
jincosghost <phaposlucl@thraml.com>
2017-04-18 18:15:33 +0100

    typo and semicolon fixes for bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,21 @@
 const Discord = require("discord.js");
-const settings = require("./settins.json");
+const settings = require("./settings.json");
 
 var bot = new Discord.Client({
-	autorun: true
+  autorun: true
 });
-var prefix = settings.prefix
+var prefix = settings.prefix;
 
 bot.on("ready", () => {
-	console.log("-------" + `Bot is ready.` + "-------");
+  console.log("-------" + `Bot is ready.` + "-------");
 });
 
 bot.on("message", msg => {
   let command = msg.content.toLowerCase().slice(prefix.length).split(" ");
 
-  if (command[0] == "hey" || "hello") {
+  if (command[0] == "hey" || command[0] == "hello") {
     return msg.channel.sendMessage(`Hello @!${msg.author.id}!`);
   }
-}
+});
 
 bot.login(settings.token);
